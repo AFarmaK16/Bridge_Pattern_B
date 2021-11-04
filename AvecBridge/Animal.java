6552ac1f2735148b948732b3b488659f9da3a536
AFarmaK16 <84871905+AFarmaK16@users.noreply.github.com>
2021-11-04 13:30:58 +0000

    Update Animal.java

--- AvecBridge/Animal.java
+++ AvecBridge/Animal.java
@@ -1,13 +1,9 @@
 public abstract class Animal {
     public AnimalFeeding feed;
-    // public int poids;
-    // public int taille;
-    // public Animal(AnimalFeeding f){
-    //     this.feed = f;
-    // }
+   
     public Animal(AnimalFeeding f){
         this.feed=f;
-        // this.poids=p;
+       
     }
     abstract public void Nourrir();
     public AnimalFeeding getFeed(){
